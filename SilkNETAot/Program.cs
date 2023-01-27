f1da2dfc53a62056de7b13b28282daa1eb264d0f
Shane <Siroro@users.noreply.github.com>
2023-01-27 02:59:33 +0000

    Change shaders to raw string literals

--- SilkNETAot/Program.cs
+++ SilkNETAot/Program.cs
@@ -83,25 +83,27 @@ internal class Program
                 BufferUsageARB.StaticDraw);
         }
 
-        const string vertexCode = @"
-#version 330 core
+        const string vertexCode = """
+            #version 330 core
 
-layout (location = 0) in vec3 aPosition;
+            layout (location = 0) in vec3 aPosition;
 
-void main()
-{
-    gl_Position = vec4(aPosition, 1.0);
-}";
+            void main()
+            {
+                gl_Position = vec4(aPosition, 1.0);
+            }
+        """;
 
-        const string fragmentCode = @"
-#version 330 core
+        const string fragmentCode = """
+            #version 330 core
 
-out vec4 out_color;
+            out vec4 out_color;
 
-void main()
-{
-    out_color = vec4(1.0, 0.5, 0.2, 1.0);
-}";
+            void main()
+            {
+                out_color = vec4(1.0, 0.5, 0.2, 1.0);
+            }
+            """;
 
         var vertexShader = _gl.CreateShader(ShaderType.VertexShader);
         _gl.ShaderSource(vertexShader, vertexCode);
